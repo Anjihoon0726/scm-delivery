a53d9e6d701109e54d617efbd1c8233dbe27183a
Jihoon <anjihoon0726@gmail.com>
2026-08-07 17:58:26 +0900

    update

--- src/main/java/com/example/scm_delivery/config/KafkaConfig.java
+++ src/main/java/com/example/scm_delivery/config/KafkaConfig.java
@@ -1,7 +1,9 @@
 package com.example.scm_delivery.config;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.security.scram.ScramLoginModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +17,11 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
-    @Value("${spring.kafka.bootstrap-servers:10.1.201.138:9092}")
+    @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
+
+    @Value("${MSK_PASSWORD:CHIlx0xjZFypwUMg8Angy3Ym}")
+    private String mskPassword;
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
@@ -24,6 +29,13 @@ public class KafkaConfig {
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        // 보안 설정 추가
+        configProps.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+        configProps.put("sasl.mechanism", "SCRAM-SHA-512");
+        configProps.put("sasl.jaas.config",
+                String.format("%s required username='DLV' password='%s';", ScramLoginModule.class.getName(), mskPassword));
+
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
